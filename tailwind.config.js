58a7b7acd04b6482657f8dbc38d95bce02147d7a
Anemaygi <gianemayumisan@gmail.com>
2023-09-27 18:58:25 -0300

    style: add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,39 +12,15 @@ export default {
 		},
 		extend: {
 			colors: {
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
-				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
-				},
-				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
-				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
-				},
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
-				},
+				background: '#0E1120',
+        secbackground: '#151525',
+				hotpink: '#ed0ee4',
+				azul1: '#07c8c5',
+				azul2: '#0e557c',
+				azul3: '#0077c2',
+				azul4: '#002fcb',
+				roxo1: '#ab58c5',
+				roxo2: '#835bc5',
 			},
 			borderRadius: {
 				lg: `var(--radius)`,
