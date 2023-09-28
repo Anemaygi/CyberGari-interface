a0f286b934c91f23e5c74dbcd72fd22f1fc9070f
Anemaygi <gianemayumisan@gmail.com>
2023-09-27 22:12:39 -0300

    add: login page (without authentication)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from '@/pages/landing';
+import LoginPage from "@/pages/login";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
 }
