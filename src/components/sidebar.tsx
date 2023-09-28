e71ba6cdf7da95e156a55277c7bceaad68cfb7f2
Anemaygi <gianemayumisan@gmail.com>
2023-09-28 00:18:11 -0300

    sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { FiSettings, FiHome, FiLogOut, FiTag } from "react-icons/fi";
-import { ReactComponent as LogoIcon } from '@/img/logo.svg';
+import LogoIcon from '@/img/logo.svg';
 
 const SideBar: React.FC = () => {
   return (
-    <div className="flex">
-      <div className="fixed top-0 left-0 h-screen w-16  m-4 rounded-3xl flex flex-col bg-[#121625] text-white shadow-md">
-      <SideBarIcon icon={<LogoIcon size="28"/>} href="a" />
-        <SideBarIcon icon={<FiHome size="28"/>} href="a" />
+    <div className="fixed top-0 left-0 h-[95%] w-16 justify-content m-4 rounded-3xl flex flex-col bg-[#121625] text-white shadow-md">
 
-        <SideBarIcon icon={<FiSettings size="28"/>} href="a" />
-        <SideBarIcon icon={<FiTag size="28"/>} href="a" />
-        <SideBarIcon icon={<FiLogOut size="28"/>} href="a" />
+      <img src={LogoIcon} alt="Logo" className="h-10 w-10 m-3" />
+      <div className="h-px bg-gradient-to-r from-rosagrad to-roxograd mx-3"></div>
+
+      <div className="flex flex-grow flex-col">
+        <SideBarIcon icon={<FiHome size="28" />} href="sidebar" />
+        <SideBarIcon icon={<FiSettings size="28" />} href="algo" />
+        <SideBarIcon icon={<FiTag size="28" />} href="a" />
       </div>
+
+      <div className="h-px bg-gradient-to-r from-rosagrad to-roxograd mx-3"></div>
+      <SideBarIcon icon={<FiLogOut size="28" />} href="a" />
     </div>
   );
 };
@@ -20,15 +24,20 @@ const SideBar: React.FC = () => {
 
 type SideBarIconProps = {
   icon: React.ReactNode;
-  href: string; 
+  href: string;
 };
 
 const SideBarIcon: React.FC<SideBarIconProps> = ({ icon, href }) => (
-  <div className="relative flex items-center justify-center h-12 w-12 m-2 shadow-lg 
-                  rounded-2xl hover:bg-gradient-to-r from-rosagrad to-roxograd text-white
-                  transition-all duration-300 ease-linear">
-    {icon}
-  </div>
+  <a href={href}>
+    <div className={`relative flex items-center justify-center h-12 w-12 m-2 shadow-lg 
+                  rounded-2xl text-white transition-all duration-300 ease-linear
+                  hover:text-roxo1 
+                  ${'/' + href === window.location.pathname ? 'bg-gradient-to-r from-rosagrad to-roxograd hover:opacity-75 hover:text-white ' : 'bg-none'}`}
+    >
+      {icon}
+
+    </div>
+  </a>
 );
 
 export default SideBar;
