2a735767853133c0f4393a0ffe99a19a22646932
Anemaygi <gianemayumisan@gmail.com>
2023-10-13 17:42:50 -0300

    feat: generalize file item

--- src/components/file-item.tsx
+++ src/components/file-item.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { FiFileText, FiTrash2, FiMaximize2 } from "react-icons/fi";
 
+
+
+
+interface FileToDelete {
+  id: string;
+  name: string;
+  lastView: string;
+}
 interface ManipuleItem {
-  id: String;
-  name: String;
-  lastView: String;
-  type: boolean;
+  file: FileToDelete;
+  action: React.ReactElement;
 }
 
 
-const FileItem: React.FC<ManipuleItem> = ({id, name, lastView, type}) => {
-  
-  
-  
-  const deleteItem = ({id}: { id: String }) => {
-    console.log("Deletou "+id);
-  };
-  
-  const uncompressItem  = ({id}: { id: String }) => {
-    console.log("Descomprimiu "+id);
-  };
+const FileItem: React.FC<ManipuleItem> = ({file, action}) => {
   
 
   return(
@@ -27,12 +23,11 @@ const FileItem: React.FC<ManipuleItem> = ({id, name, lastView, type}) => {
     <div className="w-autospace-x-2 flex flex-row align-center ">
       <div className="flex items-center text-roxo1 "><FiFileText size={24} /></div>
       <div className="flex flex-grow flex-col text-left mx-3">
-        <p className="font-bold">{name}</p>
-        <p className="text-sm">Último acesso em {lastView}</p>
+        <p className="font-bold">{file.name}</p>
+        <p className="text-sm">Último acesso em {file.lastView}</p>
       </div>
-      <div className="flex items-center"><div onClick={() => uncompressItem({ id })} className={`bg-azul3 cursor-pointer rounded-md h-8 w-8 flex items-center justify-center ${ type ? "hidden":""}`}><FiMaximize2 size={20} /></div></div>
-      <div className="flex items-center"><div onClick={() => deleteItem({ id })} className="bg-[#bb1332] ml-2 cursor-pointer rounded-md h-8 w-8 flex items-center justify-center"><FiTrash2 size={20} /></div></div>
-      
+      {action}
+            
     </div>
 
   );
