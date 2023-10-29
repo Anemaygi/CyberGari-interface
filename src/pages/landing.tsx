a9f519b57be92085ad39a396d7f29c9c0638aecd
Anemaygi <gianemayumisan@gmail.com>
2023-10-29 12:22:32 -0300

    style: responsiveness

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -29,41 +29,43 @@ const LandingPage: React.FC = () => {
             <div className="z-0 h-px w-px absolute bottom-0 left-0 rounded-full shadow-SmLightOrb bg-[#7058A3]"></div>
           </div>
           {/* Item 2 */}
+          <div className="w-fit h-fit relative">
+          <div className="z-0 h-px w-px absolute bottom-0 right-0 rounded-full shadow-SmLightOrb bg-[#7058A3]"></div>
           <div className=" z-40 min-h-full h-fit bg-[#D9D9D9]/10 grid p-10 grid-cols-1 md:grid-cols-2 justify-center items-center relative">
-            <div className="z-0 h-px w-px absolute bottom-0 right-0 rounded-full shadow-SmLightOrb bg-[#7058A3]"></div>
+            
             <div className="flex justify-center"><img src={gradientFiles} className="z-20 max-h-96 h-full" /></div>
             <div className="md:pr-24 flex flex-col m-4 justify-center md:items-start items-center">
               <h1 className="uppercase text-4xl font-black py-4 "> O que é o <b className="text-hotpink neon">Cybergari</b>? </h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pellentesque magna velit, in posuere neque molestie id. Donec dui ex, eleifend a arcu eget, semper volutpat mi. Praesent et vulputate dolor. In finibus tincidunt quam, quis semper lacus ornare id. Vivamus et odio eget leo pretium vehicula non nec mi.</p>
-              <div className="w-fit  pt-4 -ml-3"><Button title="Cadastre-se!" handleClick={()=>navigate('/login')} /></div>
+              <div className="w-fit z-40 pt-4 -ml-3"><Button title="Cadastre-se!" handleClick={()=>navigate('/login')} /></div>
             </div>
-           
+            </div>
           </div>
-          <div className="h-fit grid  grid-cols-1 md:grid-cols-2 items-center my-8">
+          <div className="h-fit grid  grid-cols-1 md:grid-cols-2 items-center my-8 p-16">
           
-          <div className="flex items-center justify-center w-full h-full"><img src={bnf1}/></div>
-          <div className="pr-24 pl-24 pb-24">
+          <div className="flex items-center justify-center w-full h-full "><img src={bnf1}/></div>
+          <div className="px-4 md:px-24 pb-24 text-center md:text-left">
             <h2 className="text-xl font-bold py-4">Tenha o controle na sua mão</h2>
             <p>Com o nosso dashboard você será capaz de ver todas as informações sobre seus arquivos e a economia que o CyberGari está gerando na sua organização.</p>
           </div>
           
           <div className="flex items-center justify-center w-full h-full md:order-2"><img src={bnf2}/></div>
-          <div className="pr-24 pl-24 pt-24 md:order-1">
-            <h2 className="text-xl font-bold py-4 ">Economize de forma simples</h2>
+          <div className="px-4 md:px-24 pb-24 md:order-1 text-center md:text-right">
+            <h2 className="text-xl font-bold py-4">Economize de forma simples</h2>
             <p>Utilizando o CyberGari, a preocupação com os custos de armazenamento pode ser mitigada de forma tranquila e progressiva, deixa isso tudo com a gente!</p>
           </div>
           
           
 
           <div className="flex items-center justify-center w-full h-full md:order-3"><img src={bnf3}/></div>
-          <div className="pr-24 pl-24 pb-24 md:order-4">
+          <div className="px-4 md:px-24 pb-24 md:order-4 text-center md:text-left">
             <h2 className="text-xl font-bold py-4">Personalize a sua experiência</h2>
             <p>A nossa plataforma te dará acesso a inúmeras configurações onde você terá o controle sobre quais informações serão coletadas para análise, além da criação de tags para nos indicar os seus arquivos de maior importância.</p>
           </div>
 
           </div>
 
-          <footer className="h-fit  bg-[#D9D9D9]/10 py-20 px-10 relative z-20">
+          <footer className="h-fit  bg-[#D9D9D9]/10 py-20 relative z-20">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-20">
               <div className="flex flex-col space-y-4">
                 <p className="font-bold">A Empresa</p>
