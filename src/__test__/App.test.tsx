d21665f015e45000b1ccb367ac859f6b581fe5d3
Anemaygi <gianemayumisan@gmail.com>
2023-11-09 09:40:09 -0300

    test: test files

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,14 @@
+
+import '@testing-library/jest-dom'
+import { render } from "@testing-library/react"
+import App from "../App"
+import React from 'react'
+
+test('demo', () => {
+    expect(true).toBe(true)
+})
+
+test("Renders the main page", () => {
+    render(<App />)
+    expect(true).toBeTruthy()
+})
